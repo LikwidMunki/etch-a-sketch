17a5f7fa8f754b855ca12a21afa4d31682cc803c
Carl Mortimer <carlmortimer@Carls-Current-MacBook.local>
2024-09-05 21:43:37 +0100

    made canvas and grid bigger

--- javascript.js
+++ javascript.js
@@ -10,6 +10,27 @@ IF: 'Change Grid Size' Button is pressed, present a prompt with input of how man
 const gridSizeBtn = document.getElementById(`gridSizeBtn`);
 const container = document.getElementById(`container`);
 
+//Set the initial 16x16 grid
+
+    for(let i=0; i < 16; i++) {
+
+        const row = document.createElement(`div`);
+        row.style.display = `flex`;
+        row.classList.add(`row`);
+
+        for(let j=0; j < 16; j++){
+            const element = document.createElement(`div`);
+            element.className = `grid-box`;
+            element.style.width = `31.25px`;
+            element.style.height = `31.25px`;
+            element.style.border = `1px black solid`;
+            element.style.boxSizing = `border-box`;
+            element.style.backgroundColor = `white`;
+            row.appendChild(element);
+        }
+        container.appendChild(row);
+    }
+
 //Function for generating prompt on button click with input
 
 gridSizeBtn.addEventListener(`click`, () => {
@@ -20,12 +41,9 @@ gridSizeBtn.addEventListener(`click`, () => {
 //Create canvas
 
 function updateGridSize(size) {
-    /* document.querySelectorAll(`.grid-box`).forEach((element) => {
-        element.remove;
-    }); */
     container.innerHTML = ``;
 
-    const boxSize = 400 / size;
+    const boxSize = 500 / size;
 
     for(let i=0; i < size; i++) {
 
